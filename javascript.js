fd69518f767c3ac56b7c6c61c0461ebdbf2d36f6
travishorrell <17819559+travishorrell@users.noreply.github.com>
2023-03-24 21:26:07 -0600

    Updated the functions and finsihed the game

--- javascript.js
+++ javascript.js
@@ -1,40 +1,74 @@
-console.log("Hello World")
+function getComputerChoice() {
 
-let computerTurn = Math.floor(Math.random() * 3)
-let computerPlay
+    let computerTurn = Math.floor(Math.random() * 3)
 
-if(computerTurn === 0)
-{
-    console.log("Rock")
-    computerPlay = "ROCK"
+    if(computerTurn === 0)
+    {
+        return "ROCK"
+    }
+    else if(computerTurn === 1)
+    {
+        return "PAPER"
+    }
+    else if(computerTurn === 2)
+    {
+        return "SCISSORS"
+    }
 }
-else if(computerTurn === 1)
+
+
+function playRound(computerPlay)
 {
-    console.log("Paper")
-    computerPlay = "PAPER"
-}
-else if(computerTurn === 2)
+    getComputerChoice()
+    computerSelection = getComputerChoice()
+    let userPlay = prompt("Rock, Paper, Scissors. Enter Your play")
+    let userSelection = userPlay.toUpperCase()
+
+    if(userSelection === computerSelection)
+    {
+        console.log("It's a tie")
+    }
+    else if(userSelection === "ROCK" && computerSelection === "PAPER")
+    {
+        console.log("You Lose! Paper beats Rock")
+    }
+    else if(userSelection === "ROCK" && computerSelection === "SCISSORS")
+    {
+        console.log("You Win! Rock beats Scissors")
+    }
+    else if(userSelection === "PAPER" && computerSelection === "ROCK")
+    {
+        console.log("You Win! Paper beats Rock")
+    }
+    else if(userSelection === "PAPER" && computerSelection === "SCISSORS")
+    {
+        console.log("You Lose! Scissors beats Paper")
+    }
+    else if(userSelection === "SCISSORS" && computerSelection === "ROCK")
+    {
+        console.log("You Lose! Rock beats Scissors")
+    }
+    else if(userSelection === "SCISSORS" && computerSelection === "PAPER")
+    {
+        console.log("You Win! Scissors beats Paper")
+    }
+    else
+    {
+        console.log("Thats not a valid play. Try again.")
+    }
+ }
+
+function game()
 {
-    console.log("Scissors")
-    computerPlay = "SCISSORS"
+    for(let i = 0; i < 5; i++)
+    {
+        console.log("Round " + (i + 1) + "!")
+        playRound()
+    }
+    console.log("Game Over")
 }
 
 game()
 
-function game(computerPlay) { 
-    let userPlay = prompt("Rock, Paper, Scissors. Enter Your play")
 
-    if(computerPlay === userPlay.toUpperCase())
-    {
-        console.log("Tie!")
-    }
-    else if(computerPlay === "ROCK" && userPlay.toUpperCase() === "PAPER")
-    {
-        console.log("You Won!")
-    }
-    else if(computerPlay === "ROCK" && userPlay.toUpperCase() === "SCISSORS")
-    {
-        console.log("You Lose!")
-    }
- }
 
