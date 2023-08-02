eb246fa54b7a55a3555a1244395a94f1d94c753c
travishorrell <17819559+travishorrell@users.noreply.github.com>
2023-08-02 11:20:29 -0600

    Added ChatGPT Code to the script papge

--- javascript.js
+++ javascript.js
@@ -1,3 +1,11 @@
+
+let humanScore = 0;
+let ComputerScore = 0;
+const buttons = document.querySelectorAll('input')
+
+console.log("welcome");
+
+
 function getComputerChoice() {
 
     let computerTurn = Math.floor(Math.random() * 3)
@@ -16,14 +24,84 @@ function getComputerChoice() {
     }
 }
 
-function playGame()
+
+function playRound(humanClick)
 {
-    let humanchoice;
+    let humanChoice = humanClick;
     let computerChoice = getComputerChoice();
-    let keepPlaying ;
+    let keepPlaying = 0;
+
+    console.log("PlayRound was ran");
+
+    document.getElementById("ResultLabel").innerHTML = "You Clicked: " + humanChoice;
 
 }
 
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playRound(button.value)
+    })
+})
 
 
+/* ChatGPT Code
+  <script>
+    var playerScore = 0;
+    var computerScore = 0;
+    var maxRounds = 5;
+    var round = 1;
 
+    function playGame(userChoice) {
+      var choices = ['rock', 'paper', 'scissors'];
+      var computerChoice = choices[Math.floor(Math.random() * 3)];
+
+      var resultMessage;
+
+      if (userChoice === computerChoice) {
+        resultMessage = "It's a tie!";
+      } else if (
+        (userChoice === "rock" && computerChoice === "scissors") ||
+        (userChoice === "paper" && computerChoice === "rock") ||
+        (userChoice === "scissors" && computerChoice === "paper")
+      ) {
+        resultMessage = "You win!";
+        playerScore++;
+      } else {
+        resultMessage = "Computer wins!";
+        computerScore++;
+      }
+
+      var resultElement = document.getElementById('result');
+      resultElement.innerHTML = `You chose ${userChoice}. Computer chose ${computerChoice}. ${resultMessage}`;
+
+      var playerScoreElement = document.getElementById('playerScore');
+      var computerScoreElement = document.getElementById('computerScore');
+
+      playerScoreElement.textContent = playerScore;
+      computerScoreElement.textContent = computerScore;
+
+      if (round === maxRounds) {
+        if (playerScore > computerScore) {
+          resultElement.innerHTML += "<br>Game over. You win!";
+        } else if (playerScore < computerScore) {
+          resultElement.innerHTML += "<br>Game over. Computer wins!";
+        } else {
+          resultElement.innerHTML += "<br>Game over. It's a tie!";
+        }
+        disableButtons();
+      } else {
+        round++;
+      }
+    }
+
+    function disableButtons() {
+      var buttons = document.getElementsByClassName('choice-btn');
+      for (var i = 0; i < buttons.length; i++) {
+        buttons[i].disabled = true;
+      }
+    }
+  </script>
+</body>
+</html>
+
+*/
